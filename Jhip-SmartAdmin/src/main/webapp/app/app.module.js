8b5c8de5d58683008b734ae6c5abab8dc6489b6f
Mohammad Nuruzzaman <dr.zaman1981@gmail.com>
2018-11-28 03:50:00 +1100

    Home Bar setup with JSON by specific language

--- Jhip-SmartAdmin/src/main/webapp/app/app.module.js
+++ Jhip-SmartAdmin/src/main/webapp/app/app.module.js
@@ -420,7 +420,7 @@ angular.module('SmartAdminWebapp', [
             if (!titleKey && $state.$current.data && $state.$current.data.pageTitle) {
                 titleKey = $state.$current.data.pageTitle;
             }
-            $translate(titleKey || 'global.title').then(function (title) {
+            $translate('global.title').then(function (title) {
                 $window.document.title = title;
             });
         };
@@ -9043,59 +9043,8 @@ angular.module('SmartAdmin.Layout').directive('speechRecognition', function ($lo
     }
 });
 
-'use strict';
-
-angular.module('SmartAdmin.Layout').directive('stateBreadcrumbs', function ($rootScope, $state) {
 
 
-    return {
-        restrict: 'EA',
-        replace: true,
-        template: '<ol class="breadcrumb"><li>Home</li></ol>',
-        link: function (scope, element) {
-
-            function setBreadcrumbs(breadcrumbs) {
-                var html = '<li>Home</li>';
-                angular.forEach(breadcrumbs, function (crumb) {
-                    html += '<li>' + crumb + '</li>'
-                });
-                element.html(html)
-            }
-
-            function fetchBreadcrumbs(stateName, breadcrunbs) {
-
-                var state = $state.get(stateName);
-
-                if (state && state.data && state.data.title && breadcrunbs.indexOf(state.data.title) == -1) {
-                    breadcrunbs.unshift(state.data.title)
-                }
-
-                var parentName = stateName.replace(/.?\w+$/, '');
-                if (parentName) {
-                    return fetchBreadcrumbs(parentName, breadcrunbs);
-                } else {
-                    return breadcrunbs;
-                }
-            }
-
-            function processState(state) {
-                var breadcrumbs;
-                if (state.data && state.data.breadcrumbs) {
-                    breadcrumbs = state.data.breadcrumbs;
-                } else {
-                    breadcrumbs = fetchBreadcrumbs(state.name, []);
-                }
-                setBreadcrumbs(breadcrumbs);
-            }
-
-            processState($state.current);
-
-            $rootScope.$on('$stateChangeStart', function (event, state) {
-                processState(state);
-            })
-        }
-    }
-});
 'use strict';
 
 angular.module('SmartAdmin.Layout').factory('lazyScript', function ($q, $http) {
